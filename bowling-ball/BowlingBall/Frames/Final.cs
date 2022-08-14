f9de3ee49457b249ff4a2ae3f9f21a6cb206f36b
dashavanthk <17163750+dashavanthk@users.noreply.github.com>
2022-08-14 14:20:20 +0530

    Need to have an Final Frame - but pretty not sure on ths scoreCalculation logic

---
+++ bowling-ball/BowlingBall/Frames/Final.cs
@@ -0,0 +1,21 @@
+﻿using BowlingBall.Track;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingBall.Frames
+{
+    public class Final : Frame
+    {
+        public Final(ITrackPins track) : base(track) { }
+        public override int FramesCount => 3;
+
+
+        public override int FrameScore(int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
